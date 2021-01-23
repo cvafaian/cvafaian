1595181fbbc82eeb7a61905b15872ff38fccb94d
cvafaian <cvafaian@umich.edu>
2021-01-23 17:55:04 -0500

    Fixed bootstrap collapsed navbar

--- my-app/src/components/NavBar.js
+++ my-app/src/components/NavBar.js
@@ -1,36 +1,23 @@
 import React from 'react';
+import{ Navbar, Nav, Button, NavDropdown}  from "react-bootstrap";
 
 function NavBar(){
 	return (
-		<nav className="navbar navbar-expand-lg navbar-light bg-light">
-			<a className="navbar-brand" id="headerBackLink" href="https://www.cvafaian.me" title="Back to opening page">
+		<Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+		  <Navbar.Brand id="headerBackLink" href="https://www.cvafaian.me" title="Back to opening page">
 				Christina Vafaian
-			</a>
-			<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-				<span className="navbar-toggler-icon"></span>
-			</button>
+			</Navbar.Brand>
 
-			<div className="collapse navbar-collapse" id="navbarSupportedContent">
-				<ul className="navbar-nav mr-auto">
-					<li className="nav-item">
-						<a className="nav-link" href="#aboutMeDiv" data-toggle="collapse" data-target=".navbar-collapse.show">About Me</a>
-					</li>
-
-					<li className="nav-item">
-						<a className="nav-link" href="#experienceDiv" data-toggle="collapse" data-target=".navbar-collapse.show">Experience</a>
-					</li>
-
-					<li className="nav-item">
-						<a className="nav-link" href="#abilitiesDiv" data-toggle="collapse" data-target=".navbar-collapse.show">Abilities</a>
-					</li>
-
-					<li className="nav-item">
-						<a className="nav-link" href="#projectsDiv" data-toggle="collapse" data-target=".navbar-collapse.show">Projects</a>
-					</li>
-
-				</ul>
-			</div>
-		</nav>
+		  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+		  <Navbar.Collapse id="responsive-navbar-nav">
+		    <Nav className="mr-auto">
+		      <Nav.Link href="#aboutMeDiv">About Me</Nav.Link>
+		      <Nav.Link href="#experienceDiv">Experience</Nav.Link>
+					<Nav.Link href="#abilitiesDiv">Abilities</Nav.Link>
+					<Nav.Link href="#projectsDiv">Projects</Nav.Link>
+		    </Nav>
+		  </Navbar.Collapse>
+		</Navbar>
 	)
 }
 
