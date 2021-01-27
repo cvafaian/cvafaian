6f0860ebeb7e6c6c718ca432b180066bc5da74a0
cvafaian <cvafaian@umich.edu>
2021-01-27 10:43:53 -0500

    Additional Alterations

--- my-app/src/components/MainContactBar.js
+++ my-app/src/components/MainContactBar.js
@@ -4,23 +4,23 @@ function MainContactBar(){
 	return (
 		<div className="mainPageLink">
 
-			 <a href="https://docs.google.com/document/d/1b_8QpuH-TzOkcwTFTLhXNsaDLf8dmQERpA54Vm0Au_o/edit?usp=sharing" target="_blank" title="See resume">
+			 <a href="https://docs.google.com/document/d/1b_8QpuH-TzOkcwTFTLhXNsaDLf8dmQERpA54Vm0Au_o/edit?usp=sharing" target="_blank" rel="noreferrer" title="See resume">
 				 <i className="fa-2x fa fa-file"></i>
 			 </a>
 
-			 <a href="mailto:cvafaian@umich.edu" target="_blank" title="Message me">
+			 <a href="mailto:cvafaian@umich.edu" target="_blank" rel="noreferrer" title="Message me">
 				 <i className="fa-2x fa fa-envelope"></i>
 			 </a>
 
-			 <a href="https://github.com/cvafaian" target="_blank"  title="Visit github profile">
+			 <a href="https://github.com/cvafaian" target="_blank"  rel="noreferrer" title="Visit github profile">
 				 <i className="fa-2x fa fa-github-square"></i>
 			 </a>
 
-			 <a href="https://www.linkedin.com/in/christina-vafaian/" target="_blank" title="Visit linkedin profile">
+			 <a href="https://www.linkedin.com/in/christina-vafaian/" rel="noreferrer" target="_blank" title="Visit linkedin profile">
 				 <i className="fa-2x fa fa-linkedin"></i>
 			 </a>
 
-			 <a id="emailLinkMain" href="mailto:cvafaian@umich.edu" target="_blank" title="Message me">
+			 <a id="emailLinkMain" href="mailto:cvafaian@umich.edu" rel="noreferrer" target="_blank" title="Message me">
 				 cvafaian@umich.edu
 			 </a>
 	 </div>
