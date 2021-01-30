0d71a8a381ce177a6bbfb1a530c325acf14c6a6f
cvafaian <cvafaian@umich.edu>
2021-01-30 01:52:25 -0500

    Fixed scrolling

--- my-app/src/components/AboutMe.js
+++ my-app/src/components/AboutMe.js
@@ -15,18 +15,11 @@ function AboutMe(){
 				I most enjoy the experience of developing projects from end-to-end and getting the opportunity to learn something new while I am at it!
 				I especially appreciate thoughtful design which results in easy to understand code and user friendly interfaces.
 			</p>
-			<p> The main object oriented languages I use and am familiar with is Python and C++, but I am always interested in learning more! </p>
-			<p> If any of this sounds interesting to you, please do connect with me using any of the social media platforms below! </p>
-			<p>
-				I graduated December 2020 from the University of Michigan with a Bachelor of Science in Computer Science.
-				I am currently seeking a full time position leaning towards software development in Michigan, US.
+			<p> The main object oriented languages I use and am familiar with is Python and C++, but I am always interested in learning more!
+					Jump to the <a id="experienceReference" href="#experienceDiv"> Experience Section</a> and learn more about what other computer languages and technologies I know.
 			</p>
-			<p>
-				I most enjoy the experience of developing projects from end-to-end and getting the opportunity to learn something new while I am at it!
-				I especially appreciate thoughtful design which results in easy to understand code and user friendly interfaces.
-			</p>
-			<p> The main object oriented languages I use and am familiar with is Python and C++, but I am always interested in learning more! </p>
 			<p> If any of this sounds interesting to you, please do connect with me using any of the social media platforms below! </p>
+
 		</div>
 	)
 }
