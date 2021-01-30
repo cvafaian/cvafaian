ef9934949141af082d253285e9c7529dba60f6f7
cvafaian <cvafaian@umich.edu>
2021-01-30 03:58:01 -0500

    Fix navbar mobile

--- my-app/src/components/NavBar.js
+++ my-app/src/components/NavBar.js
@@ -1,23 +1,25 @@
 import React from 'react';
-import{ Navbar, Nav}  from "react-bootstrap";
+import{ Navbar, Nav, Container }  from "react-bootstrap";
 
 function NavBar(){
 	return (
-		<Navbar collapseOnSelect expand="lg" bg="light" variant="light">
-		  <Navbar.Brand id="headerBackLink" href="https://www.cvafaian.me" title="Back to opening page">
-				Christina Vafaian
-			</Navbar.Brand>
+		<Container>
+			<Navbar fixed="top" collapseOnSelect expand="lg"  variant="light">
+			  <Navbar.Brand id="headerBackLink" href="https://www.cvafaian.me" title="Back to opening page">
+					Christina Vafaian
+				</Navbar.Brand>
 
-		  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-		  <Navbar.Collapse id="responsive-navbar-nav">
-		    <Nav className="mr-auto">
-		      <Nav.Link href="#aboutMeDiv">About Me</Nav.Link>
-		      <Nav.Link href="#experienceDiv">Experience</Nav.Link>
-					<Nav.Link href="#abilitiesDiv">Abilities</Nav.Link>
-					<Nav.Link href="#projectsDiv">Projects</Nav.Link>
-		    </Nav>
-		  </Navbar.Collapse>
-		</Navbar>
+			  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+			  <Navbar.Collapse id="responsive-navbar-nav">
+			    <Nav className="mr-auto">
+			      <Nav.Link href="#aboutMeDiv">About Me</Nav.Link>
+			      <Nav.Link href="#experienceDiv">Experience</Nav.Link>
+						<Nav.Link href="#abilitiesDiv">Abilities</Nav.Link>
+						<Nav.Link href="#projectsDiv">Projects</Nav.Link>
+			    </Nav>
+			  </Navbar.Collapse>
+			</Navbar>
+		</Container>
 	)
 }
 
